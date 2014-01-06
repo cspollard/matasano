a4d10a57b0ebb94a9a2b5cb8ba1c9939193fcf10
Chris <cspollard@gmail.com>
2014-01-06 16:28:21 -0500

    about to switch some stuff.

--- Crypto.hs
+++ Crypto.hs
@@ -2,3 +2,9 @@ module Crypto where
 
 xor :: Bool -> Bool -> Bool
 xor a b = (a && not b) || (b && not a)
+
+xors :: [Bool] -> [Bool] -> [Bool]
+xors bs key = zipWith xor bs (cycle key)
+
+xorsRecurse :: [Bool] -> [[Bool]] -> [Bool]
+xorsRecurse = foldl xors
