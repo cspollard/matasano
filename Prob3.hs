26521b25a49c30065042ef274c45da0a167a4f9d
Chris <cspollard@gmail.com>
2014-06-08 14:49:30 -0400

    probs 4 and 5 not working.

--- Prob3.hs
+++ Prob3.hs
@@ -1,11 +1,9 @@
 import Crypto
 import qualified Base16 as B16
-import qualified ASCII as ASCII
+import qualified ASCII
 import English
-import Data.List.Extras (argmin)
-import Control.Applicative ((<$>))
 import System.Environment (getArgs)
-import Data.List (sort)
+import Data.List (sortBy)
 import Control.Arrow ((&&&))
 
 
@@ -14,13 +12,13 @@ main = do
     let toWordN = return . B16.pack . map B16.fromChar
     dat <- toWordN =<< getLine
 
-    let keys = map ASCII.fromChar alphabet
+    let keys = map ASCII.fromChar [' '..'~']
 
-    let xord = fmap (flip xorKey dat) keys
-    let strs = filter valid $ (map ASCII.toChar . ASCII.unpack) <$> xord
+    let xord = map (`xorKey` dat) keys
+    let strs = filter valid $ map (map ASCII.toChar . ASCII.unpack) xord
 
     dict <- loadDict =<< return . head =<< getArgs
 
     case dict of
         Left s -> print s
-        Right d -> print $ reverse . sort $ map (charChi2 d &&& id) strs
+        Right d -> print $ sortBy (flip compare) $ map (charChi2 d &&& id) strs
--- Prob4.hs
+++ Prob4.hs
@@ -1,20 +1,19 @@
 import Crypto
-import Data.List.Extras (argmin)
-import Data.List (sort)
-import qualified ASCII as ASCII
+import Data.List (sortBy)
+import qualified ASCII
 import qualified Base16 as B16
 import English
 import Control.Monad (liftM)
 import Control.Arrow ((&&&))
 import System.Environment (getArgs)
-import WordN (fromBits)
 
 main :: IO ()
 main = do
     bs <- liftM lines . readFile =<< return . head . tail =<< getArgs
 
     let instrings = map (B16.pack . map B16.fromChar) bs
-    let keys = map (fromBits 8) ([32..126] :: [Int])
+
+    let keys = map ASCII.fromChar [' '..'~']
 
     let outstrings = filter valid [(map ASCII.toChar . ASCII.unpack) (k `xorKey` s) | k <- keys, s <- instrings]
 
@@ -22,8 +21,7 @@ main = do
 
     case dict of
         Left s -> print s
-        Right d -> print $ reverse . sort $ map (charChi2 d &&& id) outstrings
-        -- Right d -> print $ argmin (charChi2 d) outstrings
+        Right d -> mapM_ print $ (sortBy (flip compare) . map (charChi2 d &&& id)) outstrings
 
     -- let beststrings = reverse . sort $ map (englCharChi2 &&& id) outstrings
     -- print beststrings
