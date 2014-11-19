18a9dd52eeb306b962c3743744e1703f9f70e854
Chris Pollard <cspollard@gmail.com>
2014-11-19 21:56:54 +0000

    working on bytestring conversion.

--- Data/Crypto/WordN.hs
+++ Data/Crypto/WordN.hs
@@ -1,66 +1,21 @@
 {-# LANGUAGE FlexibleInstances #-}
 
-module WordN where
+module Data.Crypto.WordN where
 
 import qualified Data.Vector as V
 import Data.Vector (Vector, (!))
-
 import Data.Bits
-import Data.Word (Word8)
-
-type WordN = Vector Bool
-
-zeroBit :: Bits a => a
-zeroBit = clearBit (bit 0) 0
-
-toBits :: Bits a => WordN -> a
-toBits w = V.ifoldr' (\ix x y -> if x then y .|. bit ix else y) zeroBit w
-
-fromBits :: Bits a => Int -> a -> WordN
-fromBits len i = V.generate len (testBit i)
-
-shiftV :: Vector a -> a -> Int -> Vector a
-shiftV w def n = V.generate l $
-    (\ix -> let s = ix - n in
-        if s >= 0 && s < l
-            then w ! s
-            else def
-    )
-
-    where l = V.length w
-
-rotateV :: Vector a -> Int -> Vector a
-rotateV w n = if n > l
-                then rotateV w (mod n l)
-                else V.generate l $
-                    (\ix -> let s = (ix - n) `mod` l in w ! s)
-
-    where l = V.length w
+import Data.ByteString.Lazy
 
 
-nand :: Bool -> Bool -> Bool
-nand x = not . (&&) x
+-- HERE
+-- TODO
+-- convert to BS
 
-instance Bits Bool where
-    (.&.) = (&&)
-    (.|.) = (||)
-    complement = not
 
-    xor x y = (x `nand` y) && (x || y)
+nand :: Bits b => b -> b -> b
+x `nand` y = complement (x .&. y)
 
-    shift b 0 = b
-    shift _ _ = False
-
-    rotate = const
-    bitSize = const 1
-    isSigned = const False
-
-    bit = shift True
-
-    testBit = shift
-
-    popCount True = 1
-    popCount False = 0
 
 instance Bits (Vector Bool) where
     (.&.) = V.zipWith (.&.)
@@ -69,18 +24,28 @@ instance Bits (Vector Bool) where
 
     complement = fmap complement
 
-    shift = flip shiftV False
-    rotate = rotateV
+    shift v i = V.generate l (\j -> if (0 <= (j+i)) && ((j+i) < l)
+                                        then v ! (j+i)
+                                        else zeroBits
+                              )
+                    where l = V.length v
 
-    bitSize = V.length
+
+    rotate v i = V.generate l (\j -> v ! (j+i `mod` l))
+                    where l = V.length v
+
+
+    bitSizeMaybe = Just . V.length
     isSigned = const False
 
-    bit i = V.generate (i+1) (== i)
+    -- this makes a vector only long enough to set the bit...
+    bit i = V.generate (i+1) (\j -> j==i)
     testBit = (!)
 
-    popCount = V.sum . V.map popCount
+    popCount = V.foldr (\b s -> if b then s+1 else s) 0
 
 
+{-
 convertChecker :: Word8 -> Bool
 convertChecker w = (toBits . fromBits 8) w == w
 
@@ -89,3 +54,4 @@ shiftChecker w = (toBits . flip shift 3 . fromBits 8) w == flip shift 3 w
 
 rotateChecker :: Word8 -> Bool
 rotateChecker w = (toBits . flip rotate 10 . fromBits 8) w == flip rotate 10 w
+-}
